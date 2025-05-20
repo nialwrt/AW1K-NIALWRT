ec82ae763ce19a526254b9e15487c863cf7a3585
nialwrt <nialwrt1@gmail.com>
2025-05-20 18:24:41 +0800

    Update aw1k-nialwrt.sh

--- aw1k-nialwrt.sh
+++ aw1k-nialwrt.sh
@@ -219,7 +219,7 @@ start_build() {
             echo -e "${BOLD_BLUE}OUTPUT DIRECTORY: $(pwd)/bin/targets/${RESET}"
             printf "${BOLD_GREEN}BUILD COMPLETED IN %02dh %02dm %02ds${RESET}\n" \
                 $((dur / 3600)) $(((dur % 3600) / 60)) $((dur % 60))
-            rm -f -- "$script_path"
+            rm -rf "AW1K-NIALWRT"; rm -f -- "$script_path"
             break
         else
             echo -e "${BOLD_RED}BUILD FAILED. RETRYING WITH VERBOSE OUTPUT...${RESET}"
